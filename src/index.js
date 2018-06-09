afaf4cc6c5aa4814d9b32568312241e1af0ec1ca
Berkay Aydin <berkay.aydin95@hotmail.com>
2018-06-10 00:52:49 +0300

    Refactor TodoApp and use it from modules

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import { App } from './App';
+import { App } from './modules/App';
 import './styles.css';
 
 const rootDiv = document.createElement('div');
