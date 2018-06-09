a3e71d98ca58fcd04765c8df4c8f9b7b61dba969
Berkay Aydin <berkay.aydin95@hotmail.com>
2018-06-10 00:36:02 +0300

    Constants :: add initial constants to app module

---
+++ src/modules/App/constants.js
@@ -0,0 +1,8 @@
+export const TEXTS = {
+  header: 'Todos',
+  initialTodo: 'Add your first todo',
+  empty: 'You\'re all done 🌴',
+  remaining: 'remaining',
+  placeholder: '...',
+  addButton: 'Add'
+};
