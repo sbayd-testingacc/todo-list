6d226bbe25ef29bd9b7e192a981430f98ec2243d
Berkay Aydin <berkay.aydin95@hotmail.com>
2018-06-10 00:53:16 +0300

    Structure :: move styles under assets

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 
 import { App } from './modules/App';
-import './styles.css';
+import './assets/styles/styles.css';
 
 const rootDiv = document.createElement('div');
 rootDiv.id = 'root';
